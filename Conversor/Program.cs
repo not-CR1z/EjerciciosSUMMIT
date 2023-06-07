663569c6cc272770dcf2782c771c3416fd986fc9
not-CR1z <crttiandurango@gmail.com>
2023-06-07 13:13:07 -0500

    jun6

---
+++ Conversor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Conversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor Dólares a Pesos Colombianos");
+            Conversor convd = new Conversor();
+            while (convd.dolar >= 0)
+            {
+                Console.WriteLine("Por favor ingrese el valor de dólares a convertir");
+
+                try
+                {
+                    convd.dolar = float.Parse(Console.ReadLine());
+                    Console.WriteLine(convd.Dolares());
+                }
+                
+                catch(FormatException e)
+                {
+                    Console.WriteLine("igrese un valor válido");
+                }
+
+            }
+        }
+    }
+
+    class Conversor
+    {
+        public float dolar = 0;
+        public float pesos = 4.214f;
+        public float Dolares()
+        {
+            return dolar * pesos;
+        }
+    }
+}
