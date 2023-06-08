037aafa1f4e6b996ded03b3af74269626911cd3c
not-CR1z <crttiandurango@gmail.com>
2023-06-08 14:43:25 -0500

    Teorema de Pitágoras 08/06

---
+++ X&Y/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConceptosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            realizarTarea();
+        }
+        static void realizarTarea()
+        {
+            Punto distanciax = new Punto();
+            Punto distanciay = new Punto( ,100);
+            double ResultadoDistancia = distanciax.Distanciahasta(distanciay);
+            Console.WriteLine($"La distancia de los puntos es igual a :{ResultadoDistancia}");
+
+        }
+
+    }
+}
---
+++ X&Y/Punto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConceptosPOO
+{
+    class Punto
+    {
+        int x, y;
+
+        public Punto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Punto()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public double Distanciahasta(Punto nuevoPunto)
+        {
+            double xDif = this.x - nuevoPunto.x;
+            double yDif = this.y - nuevoPunto.y;
+            double resultado = Math.Sqrt(Math.Pow(xDif, 2) + Math.Pow(yDif, 2));
+            return resultado;
+        }
+    }
+}
+    
