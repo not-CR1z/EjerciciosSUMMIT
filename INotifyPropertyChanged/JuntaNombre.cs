d64135e733b5f49a34c7acf59a83bf5eb0182df6
not-CR1z <crttiandurango@gmail.com>
2023-06-28 17:18:23 -0500

    INotify ejemplo

---
+++ INotifyPropertyChanged/JuntaNombre.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropiedadINotify
+{
+    public class JuntaNombre : INotifyPropertyChanged
+    {
+
+        private string nombre, apellido, nombre_completo;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set 
+            {
+                nombre = value;
+                OnPropertyChanged("Nombre_completo");
+            }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set
+            {
+                apellido = value;
+                OnPropertyChanged("Nombre_completo");
+            }
+        }
+
+        public string Nombre_completo
+        {
+            get
+            {
+                nombre_completo = nombre + " " + Apellido;
+                return nombre_completo;
+            }
+
+            set { }
+        }
+    }
+}
